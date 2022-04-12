103d8a74029e0c4507742ed6208bd2919249810b
Akash Yadav <akashyadav@externlabs.com>
2022-04-12 15:01:37 +0530

    port issue sol-bugfix-1

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 const formate = require("date-format");
 
-const PORT = 3000 || process.env.PORT;
+const PORT = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
   // res.send(process.env);
